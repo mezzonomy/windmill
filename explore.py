b443391b0653d938edf7832412f805c3ad2c287b
Pierre Gradit <pierre.gradit@gmail.com>
2025-04-04 17:35:50 +0200

    Before optimizations of connectors and iterative building

--- explore.py
+++ explore.py
@@ -1,41 +1,88 @@
 from solver import TileSolver, Periodicity
 from itertools import product, permutations, combinations
+from datetime import datetime
+
+current = datetime.now()
+
+def stamp():
+    now = datetime.now()
+    time_interval = now - current
+    hours = time_interval.seconds // 3600
+    minutes = (time_interval.seconds % 3600) // 60
+    seconds = time_interval.seconds % 60
+    return f'%  in {hours}h{minutes}\'{seconds}"'
 
 def perfmeter(progress, cardinal, prop):
-    if (progress < 1):
-        print(' ', cardinal, progress, ' %  ', end="\r")
-    else: 
+    # if (progress < 1):
+    #     print(' ', cardinal, progress, ' %  ', end="\r")
+    # else: 
+    if True:
         progress = int(progress)
         if (not prop == progress):
-            print(' ', cardinal, progress,"%                      ", end="\r")
+            print(' ', cardinal, progress, stamp(), end="\r")
             prop = progress
     return prop
 
-def stage_information(n, locked, periodic, subperiodic):
-    print(f'After {n}x{n}:', end=" ")
+def stage_information(n, iteration, locked, periodic, subperiodic):
+    print(f'After {n}x{n} over {iteration}:', end=" ")
     if (subperiodic == 0):
         if (locked == 0):
-            print(f'{periodic} periodic')
+            print(f'{periodic} periodic', stamp())
         else:
-            print(f'{locked} locked and {periodic} periodic')
+            print(f'{locked} locked and {periodic} periodic', stamp())
     else:
         if (locked == 0):
-            print(f'{periodic} periodic and {subperiodic} subperiodic')
+            print(f'{periodic} periodic and {subperiodic} subperiodic', stamp())
         else:
-            print(f'{locked} locked, {periodic} periodic and {subperiodic} subperiodic')                    
+            print(f'{locked} locked, {periodic} periodic and {subperiodic} subperiodic', stamp())                    
 
-def explore(sets, windmill = 8, stage_information = stage_information, perfmeter = perfmeter):
+chunks = lambda a_set: [a_set[i:i + 8] for i in range(0, len(a_set), 8)]
+build  = lambda t, p, pl : ''.join([pl[p[t[i]]] for i in range(len(t))])
+
+def isminimum(t, palette, colors, size):
+    # t: sequence of eight colors
+    # palette: a string
+    # colors = range(len(palette))
+    # size = len(t)//8
+    assert len(t)%8==0
+    tile = build(t,colors, palette)
+    for p in permutations(colors):
+        ppt = build(t,p, palette)
+        for cmt in permutations(range(size)):
+            cppt =''.join([ppt[p*8:(p+1)*8] for p in cmt])
+#            cppt = 
+            for rotation in range(pow(4,size)):
+                if cppt < tile: 
+                    return False
+                for pw in range(size):
+                    if (rotation % pow(4,pw)==0):
+                        cppt = (
+                            cppt[:pw*8] + 
+                            cppt[pw*8+2:pw*8+8] + 
+                            cppt[pw*8+0:pw*8+2] +  
+                            cppt[pw*8+8:]
+                        )
+    return tile
+
+def normalize(tile):
+    palette = [_ for _ in dict([(c, None) for c in tile]).keys()]
+    colors  = range(len(palette))
+    mapping = dict(zip(palette, colors))
+    t       = [mapping[c] for c in tile]
+    return minimum(t, palette, colors, len(tile)//8)
+    
+
+def explore(sets, cardinal, stage_information = stage_information, perfmeter = perfmeter):
     n = 2
     while True:
         sieve     = [];  # kept sets
         locked    = 0; periodic  = 0; subperiodic = 0; last_rats = [] # outputs
         idx       = 0; prop      = 0                                  # performance
-        cardinal = len(sets)
         for a_set in sets:
-            idx += 1; progress = 100*idx/cardinal
-            prop = perfmeter(progress, cardinal, prop)
-            # - 
-            as_list = [a_set[i:i + windmill] for i in range(0, len(a_set), windmill)]
+            if (n>2): # perfmeter in generator
+                idx += 1; progress = 100*idx/cardinal
+                prop = perfmeter(progress, cardinal, prop)
+            as_list = chunks(a_set)
             model, period  = TileSolver(n, n, tiles = as_list).mp()
             if not model:
                 locked += 1
@@ -57,11 +104,17 @@ def explore(sets, windmill = 8, stage_information = stage_information, perfmeter
             # - End sieve for a_set at a given size
         if (periodic == 0 and locked==0 and subperiodic == 0):
             break
-        stage_information(n, locked, periodic, subperiodic)
+        stage_information(n, idx, locked, periodic, subperiodic)
         if len(sieve)==0:
             break
         n += 1
         sets = sieve
+        cardinal = len(sets)
         # - End loop
-    
     return n, sieve, last_rats
+
+if __name__ == '__main__':
+    from sys import argv
+    print(normalize(argv[-1]))
+
+# owjwjwowwoRBwoRBowjoRBwwowojRBxwBRBRBRBRwwojBBABwxojBBBAwjBBBRBAwoBBBRAB
--- explorer.py
+++ explorer.py
@@ -1,50 +1,34 @@
 # ----------------------------------------------------
 #   C I T I Z E N   P A R T 
-
-palette   = "mM"
-size      = 3
+from sys import argv
+palette   = argv[-1]
+size      = int(argv[-2])
 
 # ----------------------------------------------------
 from itertools import product, permutations
-from explore import explore, perfmeter
+from explore import explore, perfmeter, isminimum
 
-print(size, 'windmills and', f'{len(palette)} color palette:', palette)
 # windmill = 6  # triangles 
 # windmill = 12 # hexagons 
-windmill = 8  # squares
+# windmill = 8  # squares (& kites)
 
-sets    = []; scans   = {}
-build   = lambda t,p : ''.join([palette[p[t[i]]] for i in range(len(t))])
 colors  = tuple(range(len(palette)))
+cardinal = pow(len(colors), 8 * size)
+print(size, 'windmills and', f'{len(palette)} color palette: {cardinal} checks')
 
-cardinal = pow(len(colors), windmill * size)
-idx      = 0; prop     = 0
-for t in product(colors, repeat = windmill * size):
-    idx += 1
-    progress = 100*idx/cardinal
-    prop = perfmeter(progress, cardinal, prop)
-    # - 
-    for p in permutations(colors):
-        ppt = build(t,p)
-        if p==colors: # root combination
-            if ppt in scans: break
-            sets.append(ppt)
-        for cmt in permutations(range(size)):
-            cppt =''.join([ppt[p*8:(p+1)*8] for p in cmt])
-            for rotation in range(pow(4,size)):
-                scans[cppt] = None
-                for pw in range(size):
-                    if (rotation % pow(4,pw)==0):
-                        cppt = (
-                            cppt[:pw*8] + 
-                            cppt[pw*8+2:pw*8+8] + 
-                            cppt[pw*8+0:pw*8+2] +  
-                            cppt[pw*8+8:]
-                        )
-print('wrt. rotations: ', cardinal, "sets reduced to", len(sets), "sets")
+def generate():
+    idx      = 0; prop     = 0
+    for t in product(colors, repeat = 8 * size):
+        idx += 1; progress = 100*idx/cardinal
+        prop = perfmeter(progress, cardinal, prop)
+        # - 
+        a = isminimum(t, palette, colors, size)
+        if a: yield a
+
+# print('wrt. rotations: ', cardinal, "sets reduced to", len(sets), "sets")
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - 
-n, sets, last_rats = explore(sets)
+n, sets, last_rats = explore(generate(), cardinal)
 cardinal = len(sets)
 print(f'After {n}x{n}: it remains {cardinal} candidates')
 if sets:
